2249047cc377136034d7766455ae4f1470d46120
Amanda Bienz <bienz2@users.noreply.github.com>
2025-08-11 15:41:07 -0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,10 @@
+## For codespaces, needed since ProfBienz owns the src submodule
+git config --global --add safe.directory "$(pwd)"/src
+
+## Update the src folder with current remote submodule
 git submodule update --init --remote
 
+## Make build folder and compile code
 mkdir build
 cd build
 cmake ..
